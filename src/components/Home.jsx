89be3a5db62c7e591419929e7b5e40a876970e6d
navid001 <navidalvi.001@gmail.com>
2024-01-31 15:39:50 +0600

    fixed homepage responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,34 +13,32 @@ const Home = () => {
 				<img
 					src={"/Logo.png"}
 					alt="Logo"
-					className="h-20 md:h-24 hover:scale-110 transition-all duration-300 bg-white p-4 rounded-lg mb-4"
+					className="h-16 md:h-24 hover:scale-110 transition-all duration-300 bg-white p-2 md:p-4 rounded-lg mb-4"
 				/>
-				<div className="text-3xl md:text-5xl text-center font-bold my-12">
+				<div className="text-2xl md:text-3xl text-center font-bold my-6 md:my-12">
 					The Next Big Idea is Yours
 				</div>
-				<div className="flex justify-center items-center">
-					<div className="text-center border border-white rounded-lg p-6 max-w-xl space-y-4 shadow-lg mx-4 transition-all duration-300 py-16">
-						<div className="text-xl md:text-2xl font-semibold my-4">
+				<div className="flex flex-col mt-12 mb-0 md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6">
+					<div className="text-center border border-white rounded-lg p-4 md:p-6 max-w-md w-full md:w-auto">
+						<div className="text-md md:text-lg font-semibold my-4 md:my-6">
 							Ready to turn your startup idea into reality?
 						</div>
 						<Link
 							to="/apply"
-							className="text-sm md:text-lg text-white border border-white font-semibold hover:text-ribbonRed hover:scale-110 transition-all duration-300 hover:bg-white py-2 px-4 rounded-lg inline-block shadow-md my-8 mb-12">
+							className="text-xs md:text-sm text-white border border-white font-semibold hover:text-ribbonRed hover:scale-110 transition-all duration-300 hover:bg-white py-1 md:py-2 px-2 md:px-4 rounded-lg inline-block w-full md:w-auto">
 							Apply Now
 						</Link>
 					</div>
 
-					<div className="flex justify-around items-center">
-						<div className="text-center border border-white rounded-lg p-6 max-w-xl space-y-4 shadow-lg mx-4 transition-all duration-300 py-16">
-							<div className="text-xl md:text-2xl font-semibold my-4">
-								Have questions or want to explore possibilities?
-							</div>
-							<Link
-								to="/contact"
-								className="text-sm md:text-lg text-white border border-white font-semibold hover:text-ribbonRed hover:scale-110 transition-all duration-300 hover:bg-white py-2 px-4 rounded-lg inline-block shadow-md my-8 mb-12">
-								Contact Us
-							</Link>
+					<div className="text-center border border-white rounded-lg p-4 md:p-6 max-w-md w-full md:w-auto">
+						<div className="text-md md:text-lg font-semibold my-4 md:my-6">
+							Have questions or want to explore possibilities?
 						</div>
+						<Link
+							to="/contact"
+							className="text-xs md:text-sm text-white border border-white font-semibold hover:text-ribbonRed hover:scale-110 transition-all duration-300 hover:bg-white py-1 md:py-2 px-2 md:px-4 rounded-lg inline-block w-full md:w-auto">
+							Contact Us
+						</Link>
 					</div>
 				</div>
 			</motion.div>
